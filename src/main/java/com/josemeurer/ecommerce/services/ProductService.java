e270f7d96eb751f7895b54750658c151af5ae089
josemeurer <josewmeurer@gmail.com>
2022-12-11 17:39:00 -0300

    POST new product

--- src/main/java/com/josemeurer/ecommerce/services/ProductService.java
+++ src/main/java/com/josemeurer/ecommerce/services/ProductService.java
@@ -2,6 +2,7 @@ package com.josemeurer.ecommerce.services;
 
 import com.josemeurer.ecommerce.dto.ProductDTO;
 import com.josemeurer.ecommerce.models.Product;
+import com.josemeurer.ecommerce.repositories.CategoryRepository;
 import com.josemeurer.ecommerce.repositories.ProductRepository;
 import com.josemeurer.ecommerce.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,9 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Transactional(readOnly = true)
     public ProductDTO findById(UUID id) {
         Product entity = repository.findById(id)
@@ -30,5 +34,24 @@ public class ProductService {
     public Page<ProductDTO> findAllPaged(Pageable pageable) {
         Page<Product> page = repository.findAll(pageable);
         return page.map(ProductDTO::new);
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        copyDtoToEntity(dto, entity);
+
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        dto.getCategories().forEach(x ->
+                entity.getCategories().add(categoryRepository.getReferenceById(x.getId())));
     }
 }
